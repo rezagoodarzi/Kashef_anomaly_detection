880bfc88178ff2eb3a1157b1be51d78abba6ef51
rezagoodarzi <rezagoodarzi.g@gmail.com>
2025-12-17 15:58:12 +0330

    optimize anomaly part

--- src/processing/add_anomaly.py
+++ src/processing/add_anomaly.py
@@ -2,9 +2,9 @@ import pandas as pd
 import re
 
 # Read the CSV files
-df1 = pd.read_csv(r'C:\Users\USER\Desktop\kasheftest\Git_hub\Kashef_anomaly_detection\data\process\KJ_ne_site_anomaly_no_cordinate.csv')
+df1 = pd.read_csv(r'C:\Users\USER\Desktop\kasheftest\Git_hub\Kashef_anomaly_detection\data\process\KJ_finalData.csv')
 
 
 rssi_col = 'RSSI_PUCCH(EUCell_Eric)(CRA)'
-df1['Anomaly'] = df1[rssi_col] > -93
-df1.to_csv("KJ_ne_site_anomaly_no_cordinate.csv", index=False)
+df1['Anomaly'] = df1[rssi_col] > -102
+df1.to_csv("KJ_finalData_102.csv", index=False)
